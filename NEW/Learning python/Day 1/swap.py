00367c2d1f95d93ac921ed0bd26331de0edcffef
jotsingh <jotsinghkaran987@gmail.com>
2026-03-18 17:39:04 +0530

    change made

---
+++ NEW/Learning python/Day 1/swap.py
@@ -0,0 +1,9 @@
+num1=5
+num2=10
+num3=0
+num3=num1
+num1=num2
+num2=num3
+
+print(num1)
+print(num2)
---
+++ NEW/Learning python/Day 1/swapwiththrird.py
@@ -0,0 +1,16 @@
+# num1=5
+# num2=10
+
+# num1,num2= num2,num1
+
+# print(num1)
+# print(num2)
+
+num1=5
+num2=10
+num1=num2+num1 # 15
+num2=num1-num2 # 15-10=5
+num1= num1-num2 # 15-5=10
+print(num1)
+print(num2)
+
