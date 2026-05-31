1342cda21faab49092f1b2245f3c0bbf50aa3467
jotsingh <jotsinghkaran987@gmail.com>
2026-05-31 18:19:36 +0530

    change made

---
+++ new (online )/HTML/Javascript/script.js
@@ -0,0 +1,14 @@
+// var a=10
+// let b=20
+// console.log(a+b)
+
+var list = [1,2,"Hello"]
+// console.log(list)
+// var b=2
+// for (var i=0;i<3;i++){
+//     console.log(list[i])
+// }
+
+// var element = document.getElementsByClassName("container")
+// console.log(element)
+// element[0].style.background = "yellow"
